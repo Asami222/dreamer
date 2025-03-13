9ab72717230a31ad439d28ae70dafad1eec9cc59
Asami222 <asa2_pokecho@icloud.com>
2025-03-14 00:46:13 +0900

    six

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 import Layout from "components/templates/Layout"
-
+import Box from 'components/layout/Box'
+import Text from 'components/atoms/Text'
 
 const NotFoundPage: NextPage = () => {
   
   return (
     <Layout>
-      ページが見つかりません(´・ω・｀)
+      <Box $textAlign='center'>
+        <Text $color='text' $fontSize='large'>
+        ページが見つかりません
+        </Text>
+      </Box>
     </Layout>
   )
 }
