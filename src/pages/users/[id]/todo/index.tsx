22fa008e2b8c709b9577e935dae36c2949332812
Asami222 <asa2_pokecho@icloud.com>
2025-03-19 22:30:42 +0900

    fix13

--- src/pages/users/[id]/todo/index.tsx
+++ src/pages/users/[id]/todo/index.tsx
@@ -84,8 +84,8 @@ margin-top: 24px;
   border-bottom: 3px solid #D26871;
 }
 .hover-text {
-    outline-width: 1px;
-    outline-color: ${({theme}) => theme.colors.secondary};
+    background-color: none;
+    background: none;
   }
 }
 `
